a04000a9b236b50b2b28f931d14a2ba1e0852ab7
Carro Cake <gamestama@gmail.com>
2014-03-30 14:31:13 -0400

    Upsies 2
    Fixed things

--- Minigames 2.0/src/net/mcshockwave/Minigames/Commands/UpsiesCommand.java
+++ Minigames 2.0/src/net/mcshockwave/Minigames/Commands/UpsiesCommand.java
@@ -6,8 +6,26 @@ import org.bukkit.command.CommandSender;
 
 public class UpsiesCommand implements CommandExecutor {
 
+	Location red = new Location(Minigames.getDefaultWorld, 303, 139, -789)
+	Location blue = new Location(Minigames.getDefaultWorld, 304, 139, -771)
 	@Override
-	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		if (commandLabel.equalsIgnoreCase("upsies")) {
+			if (args.length == 0) {
+			sender.sendMessage("Unknown command. Type \"/help\" for help.");
+			 
+			} else {
+			if (args[0].equalsIgnoreCase("b") {
+				if (SQLTable.hasRank(p.getName()), Rank.JR_MOD) {
+					sender.teleport(blue);
+				}
+				else if (args[0].equalsIgnoreCase("r") {
+					if (SQLTable.hasRank(p.getName()), Rank.JR_MOD) {
+						sender.teleport(red);
+
+			 
+			}
+			}
 		return false;
 	}
 
