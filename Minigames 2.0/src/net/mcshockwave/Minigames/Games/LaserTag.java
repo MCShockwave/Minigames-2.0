ac4da41420d32829b687b2b513ce15841945a2a5
CarroCake <CrystalPrism@icloud.com>
2014-04-21 10:02:20 -0400

    eh

--- Minigames 2.0/src/net/mcshockwave/Minigames/Games/LaserTag.java
+++ Minigames 2.0/src/net/mcshockwave/Minigames/Games/LaserTag.java
@@ -43,8 +43,9 @@ public class LaserTag implements IMinigame {
 	private Location yb2 = new Location(Minigames.getDefaultWorld(), -1474, 105, -1);
 	private Location gb1 = new Location(Minigames.getDefaultWorld(), -1525, 105, 2);
 	private Location gb2 = new Location(Minigames.getDefaultWorld(), -1525, 105, -1);
-	private int startp = 250;
-	private int gint = 250, yint = 250;
+	
+	private int startp = 120;
+	private int gint = 120, yint = 120;
 	
 	Scoreboard s;
 	Score yscore, gscore;
