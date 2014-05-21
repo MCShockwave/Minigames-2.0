05df63256c731aa519b838352978e6256141816c
Blaxcraft <golfboy2300@yahoo.com>
2014-05-20 22:17:57 -0700

    Framework

---
+++ Minigames 2.0/src/net/mcshockwave/Minigames/Games/Ghostbusters.java
@@ -0,0 +1,20 @@
+package net.mcshockwave.Minigames.Games;
+
+import net.mcshockwave.Minigames.Events.DeathEvent;
+import net.mcshockwave.Minigames.Handlers.IMinigame;
+
+public class Ghostbusters implements IMinigame {
+
+	@Override
+	public void onGameStart() {		
+	}
+
+	@Override
+	public void onGameEnd() {		
+	}
+
+	@Override
+	public void onPlayerDeath(DeathEvent e) {		
+	}
+
+}
