4ca316b26bd8450692dd267a0a69d8534e439ab5
Blaxcraft <golfboy2300@yahoo.com>
2014-06-04 22:59:22 -0700

    hype

---
+++ Minigames 2.0/src/net/mcshockwave/Minigames/Games/Tiers.java
@@ -0,0 +1,26 @@
+package net.mcshockwave.Minigames.Games;
+
+import net.mcshockwave.Minigames.Events.DeathEvent;
+import net.mcshockwave.Minigames.Handlers.IMinigame;
+
+public class Tiers implements IMinigame {
+
+	@Override
+	public void onGameStart() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onGameEnd() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onPlayerDeath(DeathEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
