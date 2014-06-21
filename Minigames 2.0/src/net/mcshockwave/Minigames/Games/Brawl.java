606c8f87c0adb4ee3309100579f90609b9d973f2
CarroCake <CrystalPrism@icloud.com>
2014-06-21 15:37:27 -0400

    formatting and panther abuse fix

--- Minigames 2.0/src/net/mcshockwave/Minigames/Games/Brawl.java
+++ Minigames 2.0/src/net/mcshockwave/Minigames/Games/Brawl.java
@@ -1,5 +1,8 @@
 package net.mcshockwave.Minigames.Games;
 
+import net.mcshockwave.MCS.MCShockwave;
+import net.mcshockwave.MCS.SQLTable;
+import net.mcshockwave.MCS.SQLTable.Rank;
 import net.mcshockwave.MCS.Utils.ItemMetaUtils;
 import net.mcshockwave.Minigames.Game;
 import net.mcshockwave.Minigames.Minigames;
@@ -12,26 +15,31 @@ import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Sound;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerToggleFlightEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.PluginBase;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.Vector;
 
 public class Brawl implements IMinigame {
 
-	public Player	b1		= null, b2 = null;
+	public Player b1 = null, b2 = null;
 
-	Location		l1		= new Location(Minigames.getDefaultWorld(), 301.5, 134, -780, 270, 0);
-	Location		l2		= new Location(Minigames.getDefaultWorld(), 306.5, 134, -780, 90, 0);
+	Location l1 = new Location(Minigames.getDefaultWorld(), 301.5, 134, -780,
+			270, 0);
+	Location l2 = new Location(Minigames.getDefaultWorld(), 306.5, 134, -780,
+			90, 0);
 
-	public long		invin	= 0;
+	public long invin = 0;
 
 	@Override
 	public void onGameStart() {
@@ -51,7 +59,8 @@ public class Brawl implements IMinigame {
 	@Override
 	public void onPlayerDeath(DeathEvent e) {
 		if (b1 == e.p || b2 == e.p) {
-			Minigames.broadcastDeath(e.p, e.k, "%s fell off the tower", "%s was knocked off the tower by %s");
+			Minigames.broadcastDeath(e.p, e.k, "%s fell off the tower",
+					"%s was knocked off the tower by %s");
 			if (e.p == b1) {
 				b2.teleport(Game.Brawl.spawn);
 			} else {
@@ -67,7 +76,8 @@ public class Brawl implements IMinigame {
 
 	@EventHandler
 	public void onEntityDamage(EntityDamageEvent event) {
-		if (event.getEntity() instanceof Player && event.getCause() == DamageCause.FALL) {
+		if (event.getEntity() instanceof Player
+				&& event.getCause() == DamageCause.FALL) {
 			event.setCancelled(true);
 		}
 	}
@@ -76,9 +86,11 @@ public class Brawl implements IMinigame {
 		Player ret = null;
 		int times = 0;
 		while (ret == null) {
-			ret = Minigames.getOptedIn().get(rand.nextInt(Minigames.getOptedIn().size()));
+			ret = Minigames.getOptedIn().get(
+					rand.nextInt(Minigames.getOptedIn().size()));
 
-			if (ret == b1 || ret == b2 || !Minigames.alivePlayers.contains(ret.getName())) {
+			if (ret == b1 || ret == b2
+					|| !Minigames.alivePlayers.contains(ret.getName())) {
 				ret = null;
 			}
 			times++;
@@ -128,7 +140,9 @@ public class Brawl implements IMinigame {
 
 	public void specAbil(Player p) {
 		if (Minigames.hasItem(p, ShopItem.Ruthless)) {
-			p.getInventory().addItem(ItemMetaUtils.setItemName(new ItemStack(Material.STICK), "§rWooden Pole"));
+			p.getInventory().addItem(
+					ItemMetaUtils.setItemName(new ItemStack(Material.STICK),
+							"§rWooden Pole"));
 		}
 		if (Minigames.hasItem(p, ShopItem.Panther)) {
 			p.setAllowFlight(true);
@@ -143,7 +157,8 @@ public class Brawl implements IMinigame {
 		Entity e = event.getEntity();
 		Entity d = event.getDamager();
 
-		if ((b1 == e && b2 == d || b1 == d && b2 == e) && System.currentTimeMillis() >= invin) {
+		if ((b1 == e && b2 == d || b1 == d && b2 == e)
+				&& System.currentTimeMillis() >= invin) {
 			if (Minigames.hasItem((Player) d, ShopItem.Outcast)
 					&& ((Player) d).getItemInHand().getType() == Material.WOOD_SWORD) {
 				event.setDamage(1);
@@ -153,7 +168,8 @@ public class Brawl implements IMinigame {
 			if (Minigames.hasItem((Player) d, ShopItem.Ruthless)
 					&& ((Player) d).getItemInHand().getType() == Material.STICK) {
 				((Player) e).removePotionEffect(PotionEffectType.CONFUSION);
-				((Player) e).addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 100, 0));
+				((Player) e).addPotionEffect(new PotionEffect(
+						PotionEffectType.CONFUSION, 100, 0));
 			}
 		} else {
 			event.setCancelled(true);
@@ -164,11 +180,28 @@ public class Brawl implements IMinigame {
 	public void onPlayerToggleFlight(PlayerToggleFlightEvent event) {
 		Player p = event.getPlayer();
 
-		if (p.getGameMode() != GameMode.CREATIVE && Minigames.alivePlayers.contains(p.getName()) && event.isFlying()) {
+		if (p.getGameMode() != GameMode.CREATIVE
+				&& Minigames.alivePlayers.contains(p.getName())
+				&& event.isFlying()) {
 			event.setCancelled(true);
 			p.setVelocity(p.getVelocity().add(new Vector(0, 2, 0)));
-			p.getWorld().playSound(p.getLocation(), Sound.ENDERDRAGON_WINGS, 3, 1);
+			p.getWorld().playSound(p.getLocation(), Sound.ENDERDRAGON_WINGS, 3,
+					1);
 			p.setAllowFlight(false);
+		}
+	}
+
+	@EventHandler
+	public void onPlayerMove(PlayerMoveEvent e) {
+		String s =  e.getPlayer().getName();
+		if (Minigames.alivePlayers.contains(e.getPlayer().getName()) && !SQLTable.hasRank(s, Rank.JR_MOD)); {
+			if (e.getTo().getY() >= 140) {
+
+				Player p = e.getPlayer();
+				p.setHealth(0);
+				MCShockwave.send(e.getPlayer(), "Do not abuse the %s!",
+						"Panther perk");
+			}
 		}
 	}
 
