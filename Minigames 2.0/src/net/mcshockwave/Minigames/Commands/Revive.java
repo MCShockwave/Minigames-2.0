24aec39a44523afe6da4c3805c8393044d805c23
ShadowLAX <cody09061999@gmail.com>
2014-07-17 08:03:12 -0700

    Your way doesn't work properly on most games Brady. If you don't want to update it whenever we add a new game that's fine I will. Just leave it like this unless you can make a way that works.

--- Minigames 2.0/src/net/mcshockwave/Minigames/Commands/Revive.java
+++ Minigames 2.0/src/net/mcshockwave/Minigames/Commands/Revive.java
@@ -2,16 +2,25 @@ package net.mcshockwave.Minigames.Commands;
 
 import net.mcshockwave.MCS.SQLTable;
 import net.mcshockwave.MCS.SQLTable.Rank;
+import net.mcshockwave.MCS.Utils.ItemMetaUtils;
 import net.mcshockwave.Minigames.Game;
 import net.mcshockwave.Minigames.Game.GameTeam;
 import net.mcshockwave.Minigames.Minigames;
+import net.mcshockwave.Minigames.Games.*;
+import net.mcshockwave.Minigames.Shop.ShopItem;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class Revive implements CommandExecutor {
 	
@@ -42,12 +51,110 @@ public class Revive implements CommandExecutor {
 				Minigames.deadPlayers.remove(p.getName());
 				Minigames.alivePlayers.add(p.getName());
 				team.team.addPlayer(p);
-				p.damage(p.getMaxHealth());
+				p.teleport(team.spawn);
+				if (cg == Game.Airships) {
+					p.getInventory().addItem(
+							ItemMetaUtils.addEnchantment(new ItemStack(
+									Material.BOW), Enchantment.ARROW_INFINITE,
+									1));
+					p.getInventory().addItem(new ItemStack(Material.ARROW));
+					p.setAllowFlight(true);
+					p.setFlying(true);
+					p.addPotionEffect(new PotionEffect(
+							PotionEffectType.NIGHT_VISION, 10000000, 0));
+					if (Minigames.hasItem(p, ShopItem.Demoman)) {
+						p.getInventory()
+								.setItem(8, new ItemStack(Material.TNT));
+					}
+					if (Minigames.hasItem(p, ShopItem.Jammer)) {
+						p.getInventory().setItem(8,
+								new ItemStack(Material.NETHER_STAR));
+					}
+				} else if (cg == Game.Boarding) {
+					Minigames.clearInv(p);
+					PlayerInventory pi = p.getInventory();
+					pi.addItem(ItemMetaUtils.setItemName(new ItemStack(
+							Material.IRON_SWORD), ChatColor.RESET
+							+ "Steel Sword"));
+					pi.addItem(ItemMetaUtils.setItemName(new ItemStack(
+							Material.IRON_AXE), ChatColor.RESET + "Musket"));
+					pi.setItem(8, new ItemStack(Material.SULPHUR, 2));
+					if (Minigames.hasItem(p, ShopItem.Buccaneer)) {
+						p.addPotionEffect(new PotionEffect(
+								PotionEffectType.SPEED, 100000000, 0));
+						p.setAllowFlight(true);
+					}
+				} else if (cg == Game.Build_and_Fight) {
+					BuildAndFight bnf = new BuildAndFight();
+					if (team.color == ChatColor.GREEN) {
+						bnf.giveKit(p, 13);
+					}
+					if (team.color == ChatColor.YELLOW) {
+						bnf.giveKit(p, 4);
+					}
+				} else if (cg == Game.Core) {
+					Core core = new Core();
+					core.giveKit(p);
+				} else if (cg == Game.Dodgeball) {
+					Dodgeball db = new Dodgeball();
+					if (Minigames.hasItem(p, ShopItem.Athlete)) {
+						p.addPotionEffect(new PotionEffect(
+								PotionEffectType.SPEED, 10000000, 1));
+					}
+					if (Minigames.hasItem(p, ShopItem.Catcher)) {
+						db.canBeHit.add(p);
+					}
+				} else if (cg == Game.Dogtag) {
+					Dogtag dt = new Dogtag();
+					dt.giveKit(p);
+				} else if (cg == Game.Ghostbusters) {
+					Ghostbusters gb = new Ghostbusters();
+					boolean g = false;
+					if (team.color == ChatColor.DARK_GRAY) {
+						g = true;
+					}
+					gb.giveKit(p, g);
+				} else if (cg == Game.Infection) {
+					Infection i = new Infection();
+					i.giveKit(p);
+					if (Minigames.hasItem(p, ShopItem.Floater)) {
+						p.setAllowFlight(true);
+					}
+				} else if (cg == Game.Laser_Tag) {
+					LaserTag lt = new LaserTag();
+					lt.giveItems(p);
+				} else if (cg == Game.Siege) {
+					Siege si = new Siege();
+					si.giveKit(p);
+				} else if (cg == Game.Storm_The_Castle) {
+					StormTheCastle stc = new StormTheCastle();
+					stc.giveItems(p);
+				} else if (cg == Game.Tiers) {
+					Tiers ti = new Tiers();
+					ti.giveKit(team);
+				} else if (cg == Game.Village_Battle) {
+					VillageBattle vb = new VillageBattle();
+					vb.startPlayer(p, team);
+				}
 			} else {
 				Minigames.resetPlayer(p);
 				Minigames.deadPlayers.remove(p.getName());
 				Minigames.alivePlayers.add(p.getName());
-				p.damage(p.getMaxHealth());
+				if (cg == Game.Brawl) {
+					p.teleport(Game.Brawl.spawn);
+				} else if (cg == Game.Four_Corners) {
+					p.teleport(Game.Four_Corners.spawn);
+				} else if (cg == Game.Hot_Potato) {
+					p.teleport(Game.Hot_Potato.spawn);
+				} else if (cg == Game.Loot) {
+					Loot lo = new Loot();
+					lo.setRandomGear(p);
+					p.teleport(Game.Loot.spawn);
+				} else if (cg == Game.Spleef) {
+					p.getInventory().addItem(
+							new ItemStack(Material.DIAMOND_SPADE));
+					p.teleport(Game.Spleef.spawn);
+				}
 			}
 			s.sendMessage(ChatColor.GREEN + "Success!");
 		}
