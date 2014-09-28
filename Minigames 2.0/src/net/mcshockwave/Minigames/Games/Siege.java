574cca538cdc2336cef65b3d4802e77e4d567e06
ShadowLAX <cody09061999@gmail.com>
2014-09-27 19:44:18 -0700

    Fixed more color problems

--- Minigames 2.0/src/net/mcshockwave/Minigames/Games/Siege.java
+++ Minigames 2.0/src/net/mcshockwave/Minigames/Games/Siege.java
@@ -48,13 +48,13 @@ public class Siege implements IMinigame {
 	@Override
 	public void onGameStart() {
 		hp = Bukkit.getScoreboardManager().getMainScoreboard().registerNewObjective("VillagerHealth", "dummy");
-		hp.setDisplayName("§dKing Health");
+		hp.setDisplayName("¤dKing Health");
 		hp.setDisplaySlot(DisplaySlot.SIDEBAR);
 
-		yhealth = hp.getScore(Bukkit.getOfflinePlayer("§eYellow King"));
+		yhealth = hp.getScore(Bukkit.getOfflinePlayer("¤eYellow King"));
 		yhealth.setScore(startHealth);
 
-		ghealth = hp.getScore(Bukkit.getOfflinePlayer("§aGreen King"));
+		ghealth = hp.getScore(Bukkit.getOfflinePlayer("¤aGreen King"));
 		ghealth.setScore(startHealth);
 
 		Bukkit.getScheduler().runTaskLater(plugin, new Runnable() {
